203687e446fee4854fb3df32fd381a0963fc55c8
mikeborovikov <mike.borovikov@gmail.com>
2019-11-22 12:19:27 +0300

    Vue component

---
+++ PatientMenu.js
@@ -0,0 +1,33 @@
+import { PATIENT_MENU } from 'dentalsleepsolutions-common-js/src/constants/main'
+import PatientMenuElementComponent from './PatientMenuElement.vue'
+
+export default {
+  props: {
+    patientId: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    menuElements () {
+      const elements = []
+      for (let menuElement of PATIENT_MENU) {
+        const newElement = menuElement
+        if (!menuElement.hasOwnProperty('active')) {
+          newElement.active = ''
+        }
+        if (!menuElement.hasOwnProperty('activeLike')) {
+          newElement.activeLike = []
+        }
+        if (!menuElement.hasOwnProperty('legacy')) {
+          newElement.legacy = true
+        }
+        elements.push(newElement)
+      }
+      return elements
+    }
+  },
+  components: {
+    patientMenuElement: PatientMenuElementComponent
+  }
+}
